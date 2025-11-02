40c7649c7a69749b51cf8f8c4a372e347846c397
Aditya Saxena <adity09@outlook.com>
2025-11-02 13:07:43 +0530

    minor refactor

--- m_compressor/src/m_compressor/mod.rs
+++ m_compressor/src/m_compressor/mod.rs
@@ -4,16 +4,14 @@ use std::{
     path::{Path, PathBuf},
 };
 
-use crate::m_compressor::lz77::LzSymbol;
-
 mod lz77;
 
 pub struct MCompressor {
     in_file_path: PathBuf,
     out_file_path: PathBuf,
-    reader: BufReader<std::fs::File>,
 }
 
+#[derive(Debug)]
 pub enum CompressError {
     FileOpenError,
     StreamReadError(String),
@@ -21,36 +19,34 @@ pub enum CompressError {
 
 impl MCompressor {
     pub fn get_out_file_path(&self) -> &Path {
-        return &self.out_file_path;
+        &self.out_file_path
     }
 
     pub fn get_in_file_path(&self) -> &Path {
-        return &self.in_file_path;
+        &self.in_file_path
     }
 
-    pub fn new(in_file_path_str: &str) -> Result<MCompressor, CompressError> {
-        let in_path: PathBuf = PathBuf::from(in_file_path_str);
-
-        let file: File = File::open(&in_path).map_err(|err| {
-            eprintln!("Error: {}", err);
-
-            return CompressError::FileOpenError;
-        })?;
+    pub fn new(in_file_path: impl AsRef<Path>) -> Self {
+        let in_path = in_file_path.as_ref().to_path_buf();
         let mut out_path_os_string = in_path.as_os_str().to_owned();
 
         out_path_os_string.push(".mc");
 
-        let mut self_obj = Self {
+        Self {
             in_file_path: in_path,
             out_file_path: PathBuf::from(out_path_os_string),
-            reader: BufReader::with_capacity(lz77::READER_CAPACITY, file),
-        };
-        self_obj.compress()?;
-        return Ok(self_obj);
+        }
     }
 
-    fn compress(&mut self) -> Result<(), CompressError> {
-        let symbols: Vec<LzSymbol> = lz77::process_lz77(&mut self.reader)?;
-        return Ok(());
+    pub fn compress(&self) -> Result<(), CompressError> {
+        let in_file = File::open(&self.in_file_path).map_err(|err| -> CompressError {
+            eprintln!("Error: {}", err);
+            return CompressError::FileOpenError;
+        })?;
+
+        let mut reader = BufReader::with_capacity(lz77::READER_CAPACITY, in_file);
+
+        let symbols = lz77::process_lz77(&mut reader)?;
+        Ok(())
     }
 }
--- m_compressor/src/main.rs
+++ m_compressor/src/main.rs
@@ -8,8 +8,9 @@ fn main() {
     match std::io::stdin().read_to_string(&mut file_path) {
         Ok(_) => {
             let trimmed_path = String::from(file_path.trim());
+            let m_comp = m_compressor::MCompressor::new(&trimmed_path);
 
-            if let Ok(m_comp) = m_compressor::MCompressor::new(&trimmed_path) {
+            if m_comp.compress().is_ok() {
                 println!(
                     "The compressed file will be at: {:?}.",
                     m_comp.get_out_file_path()
