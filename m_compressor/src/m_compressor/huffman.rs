e4278a80bd817afd4e981794917395a8ead3f4c0
Aditya Saxena <adity09@outlook.com>
2025-11-21 02:17:56 +0530

    bitwriter rewritten; huffman coding implementation near completion;

--- m_compressor/src/m_compressor/huffman.rs
+++ m_compressor/src/m_compressor/huffman.rs
@@ -1,20 +1,49 @@
-use std::{cmp::Reverse, collections::{BinaryHeap, VecDeque}};
+use std::{
+    cmp::Reverse,
+    collections::{BinaryHeap, VecDeque},
+};
 
 use crate::{
     constants,
     m_compressor::{CompressError, lz77::LzSymbol},
-    utils::{
-        bit_writer::BitWriter,
-        huffman_tree_node::HuffmanTreeNode,
-    },
+    utils::{bit_writer::BitWriter, huffman_tree_node::HuffmanTreeNode},
 };
 
 type LitLenCntArr = [usize; 286];
 type DistCntArr = [usize; 30];
-type HuffmanCode = (u16, u8, u8, u16);
+type CanonicalCodesMapEntry = (u128, u8);
 
-fn get_hm_code_for_lz_symbol(dist: &u16, len: &u16) -> HuffmanCode {
-    todo!()
+fn get_len_dist_code_util(len_dist: u16, codes: &Vec<u16>, ind: usize) -> u16 {
+    let mut in_range: bool = codes[ind] == len_dist;
+    in_range = in_range || (ind + 1 == codes.len());
+    in_range = in_range || (codes[ind + 1] > len_dist);
+
+    return if in_range { codes[ind] } else { 0 };
+}
+
+fn get_hm_code_for_lz_ptr(dist: &u16, len: &u16) -> (u16, u16) {
+    let mut len_code = 0;
+    let mut dist_code = 0;
+    let dist_base_codes_vec = constants::DIST_BASE_CODES.to_vec();
+    let len_base_codes_vec = constants::LEN_BASE_CODES.to_vec();
+    let sz_len = dist_base_codes_vec.len();
+    let sz_dist = len_base_codes_vec.len();
+
+    for i in 0..sz_len.max(sz_dist) {
+        if len_code > 0 && dist_code > 0 {
+            break;
+        }
+
+        if i < sz_len {
+            len_code = len_code | get_len_dist_code_util(*len, &len_base_codes_vec, i);
+        }
+
+        if i < sz_dist {
+            dist_code = dist_code | get_len_dist_code_util(*dist, &dist_base_codes_vec, i);
+        }
+    }
+
+    return (dist_code, len_code);
 }
 
 fn put_lit_len_dist_freq(
@@ -25,8 +54,9 @@ fn put_lit_len_dist_freq(
     for symbol in lz_symbols {
         match symbol {
             LzSymbol::Literal(lit) => lit_len_cnt[*lit as usize] += 1,
+
             LzSymbol::Pointer { dist, len } => {
-                let (dist_code, _, len_code, _) = get_hm_code_for_lz_symbol(dist, len);
+                let (dist_code, len_code) = get_hm_code_for_lz_ptr(dist, len);
                 lit_len_cnt[len_code as usize] += 1;
                 dist_cnt[dist_code as usize] += 1;
             }
@@ -54,8 +84,38 @@ fn get_huffman_tree(arr: &Vec<usize>) -> HuffmanTreeNode {
         heap.push(Reverse(new_node));
     }
     let Reverse(root) = heap.pop().unwrap();
-    
+
     return root;
+}
+
+fn map_canonical_codes_to_lz_symbols(
+    canonical_codes_map: &mut Vec<CanonicalCodesMapEntry>,
+    tree_node: HuffmanTreeNode,
+    path: u128,
+    height: u8,
+) {
+    if tree_node.is_leaf() {
+        canonical_codes_map[tree_node.symbol.unwrap()] = (path, height);
+    }
+
+    if tree_node.left.is_some() {
+        map_canonical_codes_to_lz_symbols(
+            canonical_codes_map,
+            *tree_node.left.unwrap(),
+            path,
+            height + 1,
+        );
+    }
+
+    if tree_node.right.is_some() {
+        let new_path: u128 = path | (1 << height);
+        map_canonical_codes_to_lz_symbols(
+            canonical_codes_map,
+            *tree_node.right.unwrap(),
+            new_path,
+            height + 1,
+        );
+    }
 }
 
 /// Produces a bit-stream corresponding to the LzSymbol sequence,
@@ -71,10 +131,16 @@ pub fn process_huffman(
 ) -> Result<(), CompressError> {
     let mut lit_len_cnt: LitLenCntArr = [0; 286];
     let mut dist_cnt: DistCntArr = [0; 30];
+    let mut lit_len_canonical_map: Vec<CanonicalCodesMapEntry> = Vec::new();
+    let mut dist_canonical_map: Vec<CanonicalCodesMapEntry> = Vec::new();
 
     put_lit_len_dist_freq(&mut lit_len_cnt, &mut dist_cnt, lz_symbols);
+
     let lit_len_tree_head_node: HuffmanTreeNode = get_huffman_tree(&lit_len_cnt.to_vec());
     let dist_tree_head_node: HuffmanTreeNode = get_huffman_tree(&dist_cnt.to_vec());
+
+    map_canonical_codes_to_lz_symbols(&mut lit_len_canonical_map, lit_len_tree_head_node, 0, 0);
+    map_canonical_codes_to_lz_symbols(&mut dist_canonical_map, dist_tree_head_node, 0, 0);
 
     Ok(())
 }
--- m_compressor/src/utils/bit_writer.rs
+++ m_compressor/src/utils/bit_writer.rs
@@ -1,53 +1,80 @@
+/*
+ * This module implements a bit level writer.
+ * Akin to how BufWriter internally buffers the written items,
+ * BitWriter accumulates bits from multiple calls.
+ * As soon as the accumulation results to a byte,
+ * the byte is reset after being flushed to the BufWriter.
+ */
+
 use std::{
     fs::File,
-    io::{BufWriter, Write},
+    io::{self, BufWriter, Write},
 };
 
+static BIT_COUNT_LIMIT: u8 = 8;
+static BUFFER_CAPACITY: usize = 64 * 1024;
+
+#[derive(Debug)]
 pub struct BitWriter {
     writer: BufWriter<File>,
-    accumulator: u64,
+    buffer: u8,
     bit_count: u8,
 }
 
 impl BitWriter {
-    pub fn new(out_file: File) -> Self {
+    pub fn new(file: File) -> Self {
         Self {
-            writer: BufWriter::new(out_file),
-            accumulator: 0,
+            writer: BufWriter::with_capacity(BUFFER_CAPACITY, file),
+            buffer: 0,
             bit_count: 0,
         }
     }
 
-    pub fn write(&mut self, token: u16, bit_cnt: u8) {
-        self.accumulator = (self.accumulator << bit_cnt) | (token as u64);
-        self.bit_count += bit_cnt;
+    /// Resets the accumulated byte,
+    /// after transferring its bits to the BufWriter.
+    fn flush_to_writer(&mut self) -> io::Result<()> {
+        self.writer.write_all(&[self.buffer])?;
+        self.buffer = 0;
+        self.bit_count = 0;
 
-        while self.bit_count >= 8 {
-            let shift = self.bit_count - 8;
-            let byte = (self.accumulator >> shift) as u8;
-            // TODO: handle error properly
-
-            let _ = self.writer.write(&[byte]);
-            self.bit_count -= 8;
-            // Mask out the written bits to keep accumulator clean, though not strictly necessary if we shift correctly next time
-            // But it's good practice to keep it clean or just rely on the shift logic.
-            // With u64 accumulator, we have plenty of space.
-            // Let's just keep the lower bits.
-            let mask = (1 << self.bit_count) - 1;
-            self.accumulator &= mask;
-        }
+        return Ok(());
     }
 
-    pub fn flush(&mut self) {
-        if self.bit_count > 0 {
-            let shift = 8 - self.bit_count;
-            let byte = (self.accumulator << shift) as u8;
+    /// Cycles through a procedure of writing a number of bits
+    /// to the accumulator byte, and fliushing the accumulator
+    /// byte to the BufWriter as soon as it has accumulated 8 bits.
+    pub fn write_bits(&mut self, value: u128, num_bits: u8) -> io::Result<()> {
+        for i in (0..num_bits).rev() {
+            let is_bit_set = (value >> i) & 1 == 1;
 
-            let _ = self.writer.write(&[byte]);
-            self.accumulator = 0;
-            self.bit_count = 0;
+            if is_bit_set {
+                let bit_mask = 1 << (BIT_COUNT_LIMIT - 1 - self.bit_count);
+                self.buffer |= bit_mask
+            }
+            self.bit_count += 1;
+
+            if self.bit_count == BIT_COUNT_LIMIT {
+                self.flush_to_writer()?;
+            }
         }
 
-        let _ = self.writer.flush();
+        return Ok(());
+    }
+
+    /// Flushes the accumulator byte
+    /// to the BufWriter and then the latter.
+    pub fn flush_all(&mut self) -> io::Result<()> {
+        if self.bit_count > 0 {
+            self.flush_to_writer()?;
+        }
+        self.writer.flush()?;
+
+        return Ok(());
+    }
+}
+
+impl Drop for BitWriter {
+    fn drop(&mut self) {
+        self.flush_all();
     }
 }
